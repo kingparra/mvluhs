59e5b009efb011c6e1a7473500c5c50fdd36e4bf
Chris King-Parra <chris@kingparra.work>
2021-04-01 14:51:19 -0400

    mvluhs now only modifies the last path segment

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,18 @@
 module Main where
 import System.Directory (renamePath)
+import System.FilePath (splitPath, (</>))
 import System.Environment (getArgs)
 import Data.Char (toLower, isSpace)
+
+isGoodChar = (`elem` "1234567890abcdefghijklmnopqrstuvwxyz.-_")
+lowerUnderscore = map (toLower . (\c -> if isSpace c
+                                        then '_'
+                                        else c))
+sanitize x = filter isGoodChar $ lowerUnderscore x
 
 main :: IO ()
 main = do
   args <- getArgs
-  mapM_ (\x -> renamePath x (sanitize x)) args
-  where
-    lowerUnderscore = map (toLower . (\c -> if isSpace c then '_' else c))
-    sanitize x = filter (`elem` "1234567890abcdefghijklmnopqrstuvwxyz.-_") (lowerUnderscore x)
+  mapM_ (\x -> renamePath x ((concat $ init $ splitPath x) </>
+                             (sanitize $ last $ splitPath x))
+        ) args
